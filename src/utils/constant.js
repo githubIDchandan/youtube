fad1f8380354a0162870613ebc40b240ee25b342
chandanK <iamchandankumar02@gmail.com>
2024-11-20 23:20:42 +0530

    demo

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 
 
-const API_KEY="";
+const API_KEY=" ";
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key="+API_KEY;
 export const OFFSET_LIVE_CHAT=15;
 
